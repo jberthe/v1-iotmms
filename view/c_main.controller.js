e0f4c73e368cc78d76cf7c3ffeb9458aa5a6d97a
p175998trial <joseph.berthe@gmail.com>
2015-07-17 11:40:42 +0000

    Initial load

---
+++ view/c_main.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("ch.saphirnet.view.c_main", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf ch.saphirnet.view.v_main
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf ch.saphirnet.view.v_main
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf ch.saphirnet.view.v_main
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf ch.saphirnet.view.v_main
+*/
+//	onExit: function() {
+//
+//	}
+
+});
